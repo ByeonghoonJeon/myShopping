421d748bbbf1e960ce4d54c0ddc80eced8dd780d
Nathan Jeon <jeon4972@snu.ac.kr>
2021-11-11 15:52:08 -0500

    Practice

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.titles}>
-      <Text style={styles.title}>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View style={styles.mainContainer}>
+        <ImageBackground
+          source={require("./assets/main.jpg")}
+          style={styles.image}
+        />
+        <View style={styles.titles}>
+          <Text style={styles.title}>로또운 분석</Text>
+          <Text style={styles.subTitle}>
+            사주 분석으로 나만의 번호와 날짜를 받아보세요
+          </Text>
+
+          <StatusBar style="auto" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,13 +30,29 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  mainContainer: {
+    width: "100%",
+    height: "100%",
+  },
   titles: {
-    marginTop: "30%",
+    marginTop: "90%",
     width: "100%",
     alignItems: "center",
   },
   title: {
     fontSize: 40,
     fontWeight: "bold",
+    color: "#fff",
+  },
+  subTitle: {
+    fontSize: 15,
+    marginTop: "2%",
+    color: "#fff",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
   },
 });
