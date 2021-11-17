eb65c283850bfbd8741fc50a96028e3fd948f06b
Nathan Jeon <jeon4972@snu.ac.kr>
2021-11-17 14:43:50 -0500

    Working on

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
 import MainScreen from "./components/MainScreen";
+import HomeScreen from "./components/Home/home";
+
+// const { Navigator, Screen } = createStackNavigator();
+
+// const App = () => {
+//   <NavigationContainer>
+//     <Navigator headerMode="name" initialRouteName="Main">
+//       <Screen name="Main" component={MainScreen}></Screen>
+//       <Screen name="Home" component={HomeScreen}></Screen>
+//     </Navigator>
+//   </NavigationContainer>;
+// };
 
 export default function App() {
   return <MainScreen />;
 }
+
+// export default App;
