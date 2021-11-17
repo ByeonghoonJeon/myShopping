d7608a65a43f3f65c6aaccc05de98b2a731645db
Nathan Jeon <jeon4972@snu.ac.kr>
2021-11-17 16:00:11 -0500

    Fixing

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 import React from "react";
-// import { createStackNavigator } from "react-navigation-stack";
-// import { NavigationContainer } from "@react-navigation/native";
 
-import MainScreen from "./components/MainScreen";
-import HomeScreen from "./components/Home/home";
+import MainScreen from "./components/MainScreen/index";
 
-// const { Navigator, Screen } = createStackNavigator();
-
-// const App = () => {
-//   <NavigationContainer>
-//     <Navigator headerMode="name" initialRouteName="Main">
-//       <Screen name="Main" component={MainScreen}></Screen>
-//       <Screen name="Home" component={HomeScreen}></Screen>
-//     </Navigator>
-//   </NavigationContainer>;
-// };
-
-export default function App() {
+const App = () => {
   return <MainScreen />;
-}
+};
+
+export default App;
