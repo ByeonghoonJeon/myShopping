c42d7afe6601304b6a1d79d4d95be522f1650685
Nathan Jeon <jeon4972@snu.ac.kr>
2021-11-30 17:53:21 -0500

    Home Screen Working

--- components/HomeComponent/HomeComponent.js
+++ components/HomeComponent/HomeComponent.js
@@ -1,6 +1,21 @@
 import React from "react";
-import { Text, View, ScrollView, ImageBackground } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import {
+  Card,
+  CardTitle,
+  CardContent,
+  CardAction,
+  CardButton,
+  CardImage,
+} from "react-native-cards";
 import HomeStyles from "./HomeComponentStyles";
+import Footer from "../Footer/FooterComponent";
 
 const HomeComponent = ({ navigation }) => {
   return (
@@ -13,7 +28,39 @@ const HomeComponent = ({ navigation }) => {
         <Text style={HomeStyles.greeting}>
           Welcome "USER NAME", {" \n"}How do you feel today?
         </Text>
+
+        <Card>
+          <Text style={HomeStyles.cardSubtitle}>My Point </Text>
+          <Text style={HomeStyles.point}>3,200 P</Text>
+
+          <CardAction separator={true} inColumn={false}>
+            <CardButton
+              onPress={() => {
+                console.warn("Hey");
+              }}
+              title="Rewards"
+              color="#FEB557"
+              style={{ marginLeft: "auto" }}
+            />
+            <CardButton
+              onPress={() => {
+                console.warn("Hey");
+              }}
+              title="Learn More"
+              color="#FEB557"
+            />
+            <CardButton
+              onPress={() => {
+                console.warn("Hey");
+              }}
+              title="View Details"
+              color="#FEB557"
+              style={{ marginRight: 15 }}
+            />
+          </CardAction>
+        </Card>
       </View>
+      <Footer />
     </ScrollView>
   );
 };
--- components/HomeComponent/HomeComponentStyles.js
+++ components/HomeComponent/HomeComponentStyles.js
@@ -1,23 +1,37 @@
 import { StyleSheet } from "react-native";
 const HomeStyles = StyleSheet.create({
   scrollContainer: {
-    flex: 1,
     backgroundColor: "#2B2B2B",
+    marginTop: 70,
   },
   container: {
-    alignItems: "flex-start",
-    marginTop: 100,
-    marginLeft: 20,
+    marginBottom: 0,
+    marginTop: 30,
+    paddingBottom: 20,
   },
   greeting: {
     color: "white",
     fontWeight: "bold",
+    marginLeft: 20,
   },
   image: {
     width: "100%",
     height: "100%",
     resizeMode: "cover",
     position: "absolute",
+  },
+  cardSubtitle: {
+    color: "grey",
+    marginLeft: "auto",
+    marginRight: 20,
+    marginTop: 20,
+  },
+  point: {
+    marginLeft: "auto",
+    marginRight: 20,
+    marginBottom: 15,
+    paddingBottom: 0,
+    fontSize: 30,
   },
 });
 
